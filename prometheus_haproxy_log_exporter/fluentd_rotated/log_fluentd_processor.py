bdfa5d4c750b18a89190c3289a5ff27337a4f156
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2020-05-26 16:28:37 +0200

    Implement support for fluentd files with random hash

--- prometheus_haproxy_log_exporter/fluentd_rotated/log_fluentd_processor.py
+++ prometheus_haproxy_log_exporter/fluentd_rotated/log_fluentd_processor.py
@@ -1,6 +1,8 @@
+import os
 import tailhead
 import time
 import json
+import glob
 from ..log_processing import AbstractLogProcessor
 
 
@@ -10,8 +12,30 @@ class LogFluentdProcessor(AbstractLogProcessor):
         self.path = path
         self.should_exit = False
 
+    def _get_path_by_pattern(self, pattern):
+        files = glob.glob(pattern)
+        if len(files) > 1:
+            raise RuntimeError("Too many files matching provided glob")
+        elif len(files) == 0:
+            return None
+        print("Matched file by pattern: {}".format(files[0]))
+        return files[0]
+
+    def follow_rotated_fluentd_path(self, pattern):
+        while True:
+            path = self._get_path_by_pattern(pattern)
+            if path is None:
+                time.sleep(0.1)
+                continue
+            for line in tailhead.follow_path(path):
+                if line is not None:
+                    yield line
+                if not os.path.exists(path):
+                    break
+                yield None
+
     def run(self):
-        for line in tailhead.follow_path(self.path):
+        for line in self.follow_rotated_fluentd_path(self.path):
             if line is None:
                 time.sleep(0.1)
                 if self.should_exit:
