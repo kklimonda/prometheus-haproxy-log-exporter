d812052b2bf949f8f83f81bdc33a71d31155fb7a
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2020-05-27 02:34:16 +0200

    Rewrite _get_path_by_pattern to take into account how fluentd works

--- prometheus_haproxy_log_exporter/fluentd_rotated/log_fluentd_processor.py
+++ prometheus_haproxy_log_exporter/fluentd_rotated/log_fluentd_processor.py
@@ -10,16 +10,41 @@ class LogFluentdProcessor(AbstractLogProcessor):
     def __init__(self, metric_updaters, path, *args, **kwargs):
         super().__init__(metric_updaters, *args, **kwargs)
         self.path = path
+        self._processed_files = []
         self.should_exit = False
 
     def _get_path_by_pattern(self, pattern):
-        files = glob.glob(pattern)
-        if len(files) > 1:
-            raise RuntimeError("Too many files matching provided glob")
-        elif len(files) == 0:
+        matched_paths = glob.glob(pattern)
+        # if no files match glob, return None and wait for the
+        # next iteration - fluentd is probably rotating file right now.
+        if len(matched_paths) == 0:
+            print("Glob matched no paths. Returning early "
+                  "and waiting for the next iteration")
             return None
-        print("Matched file by pattern: {}".format(files[0]))
-        return files[0]
+        print("Glob found {} matched paths:".format(len(matched_paths)))
+        # compare all matched paths against the list of paths already processed
+        # so that we can avoid tailing "closed" log file that is still waiting
+        # for the rotation.
+        # XXX: if self._processed_files is empty, we should compare file stat
+        # and pick least recently changed one.
+        new_path = None
+        for path in matched_paths:
+            if path in self._processed_files:
+                print("\t{} already seen - skipping".format(path))
+                continue
+            print("\t{} is a new path - tailing it".format(path))
+            new_path = path
+            break
+        # all logs have been processed, so return None and we'll try to match log
+        # file in the next iteration.
+        if new_path is None:
+            print("Glob found no paths that weren't seen before. "
+                  "Returning and waiting for the next iteration")
+            return None
+        # keep paths for the last 24 processed files (24 hours) so that we can
+        # reprocessing finished logs that have yet to been rotated.
+        self._processed_files = self._processed_files[-23:] + [new_path]
+        return new_path
 
     def follow_rotated_fluentd_path(self, pattern):
         while True:
